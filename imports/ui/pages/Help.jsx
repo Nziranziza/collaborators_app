af2af322469088221b69a996133494d797d00a8f
Nziranziza <nziranzizadaniel@gmail.com>
2018-08-29 07:43:59 +0300

    Nairobi

--- imports/ui/pages/Help.jsx
+++ imports/ui/pages/Help.jsx
@@ -15,53 +15,53 @@ function Help() {
                     <div className="wrapper">
                         <div className="col-md-3 col-sm-6 col-xs-12">
                             <div className="service-item text-center">
-                                <i className="fa fa-pencil"><img src="recruit.png" width="150" height="150"/></i>
-                                <h5><b>Register</b></h5>
+                                <i className="fa fa-pencil"><img src="membership.png" width="150" height="150"/></i>
+                                <h5><b>1.Register</b></h5>
                                 <div className="separator2"></div>
-                                <p>We Job recruiters hire the workers with our fast omatching system.</p>
+                                <p>Create an account with us.</p>
                             </div>
                         </div> 
 
                         <div className="col-md-3 col-sm-6 col-xs-12">
                             <div className="service-item text-center">
-                                <i className="fa fa-camera-retro"><img src="post-it.png" width="150" height="150"/></i>
-                                <h5><b>Complete the profile</b></h5>
+                                <i className="fa fa-camera-retro"><img src="completed.png" width="150" height="150"/></i>
+                                <h5><b>2.Complete the profile</b></h5>
                                 <div className="separator2"></div>
-                                <p>Here you can post an announcement for your job and we give the employees within a few minutes</p>
+                                <p>Go on and complete your profile</p>
                             </div>
                         </div> 
 
                         <div className="col-md-3 col-sm-6 col-xs-12">
                             <div className="service-item text-center">
                                 <i className="fa fa-lightbulb-o"><img src="career.png" width="150" height="150"/></i>
-                                <h5><b>Learn</b></h5>
+                                <h5><b>3.Learn</b></h5>
                                 <div className="separator2"></div>
-                                <p>We help people build their career by providing all the information that is specific to them</p>
+                                <p>Learn with recommended lessons</p>
                             </div>
                         </div> 
 
                         <div className="col-md-3 col-sm-6 col-xs-12">
                             <div className="service-item text-center">
                                 <i className="fa fa-lock"><img src="training.png" width="150" height="150"/></i>
-                                <h5><b>Attend the trainings</b></h5>
+                                <h5><b>4.Attend the trainings</b></h5>
                                 <div className="separator2"></div>
-                                <p>With our platform you will have to attend different trainings and online learning that will enhance your experience to the work</p>
+                                <p>Attend in-person trainings to enhance your experience</p>
                             </div>
                         </div>
                         <div className="col-md-3 col-sm-6 col-xs-12">
                             <div className="service-item text-center">
-                                <i className="fa fa-lock"><img src="training.png" width="150" height="150"/></i>
+                                <i className="fa fa-lock"><img src="post-it.png" width="150" height="150"/></i>
                                 <h5><b>Get posted</b></h5>
                                 <div className="separator2"></div>
-                                <p>With our platform you will have to attend different trainings and online learning that will enhance your experience to the work</p>
+                                <p>Now you will be posted in recruit page</p>
                             </div>
                         </div>
                         <div className="col-md-3 col-sm-6 col-xs-12">
                             <div className="service-item text-center">
-                                <i className="fa fa-lock"><img src="training.png" width="150" height="150"/></i>
+                                <i className="fa fa-lock"><img src="recruit.png" width="150" height="150"/></i>
                                 <h5><b>Be hired</b></h5>
                                 <div className="separator2"></div>
-                                <p>With our platform you will have to attend different trainings and online learning that will enhance your experience to the work</p>
+                                <p>Potential employers will be able to recruit you via our platform</p>
                             </div>
                         </div>   
 
