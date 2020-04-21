da8096b91018910292b78c046a38432467e4ac8b
Nziranziza <nziranzizadaniel@gmail.com>
2020-04-21 14:17:13 +0200

    - redesign homepage - add mock data - add mock pictures

---
+++ __mocks__/howItWorks.js
@@ -0,0 +1,51 @@
+export default {
+  jobSeeker: [
+    {
+      title: 'Register',
+      description: 'Create an account with us',
+      imageUrl: 'membership.png',
+    },
+    {
+      title: 'Complete profile',
+      description: 'Go on and complete your profile',
+      imageUrl: 'completed.png'
+    },
+    {
+      title: 'Learn',
+      description: 'Learn with recommended lessons',
+      imageUrl: 'career.png'
+    },
+    {
+      title: 'Attend trainings',
+      description: 'Attend in-person trainings to enhance your experience',
+      imageUrl:'training.png'
+    },
+    {
+      title: 'Get posted',
+      description: 'Now you will be posted in recruit page',
+      imageUrl: 'post-it.png'
+    },
+    {
+      title: 'Be hired',
+      description: 'Potential employers will be able to recruit you via our platform',
+      imageUrl: 'recruit.png'
+    }
+  ],
+  employer: [
+    {
+      title: 'View profile',
+      description: 'Access the recruit page and view profiles of job seekers',
+      imageUrl: 'view.png'
+    },
+    {
+      title: 'Hire',
+      description: 'Hire if you like',
+      imageUrl: 'recruit.png'
+    },
+    {
+      title: 'Describe',
+      description: 'Access the recruit page and view profiles of job seekers',
+      imageUrl: 'desc.png'
+    }
+  ]
+}
---
+++ __mocks__/user.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    imageUrl:'black.jpg',
+    occupation: 'Software Developer',
+    name: 'John Smith'
+  },
+  {
+    imageUrl:'miami.jpg',
+    occupation: 'Software Developer',
+    name: 'John Doe'
+  },
+  {
+    imageUrl:'Jane.jpg',
+    occupation: 'Software Developer',
+    name: 'Jane Doe'
+  },
+  {
+    imageUrl:'John.jpg',
+    occupation: 'Software Developer',
+    name: 'Peter Doe'
+  },
+  {
+    imageUrl:'doe.jpg',
+    occupation: 'Software Developer',
+    name: 'Peter Smith'
+  },
+  {
+    imageUrl:'joy.jpg',
+    occupation: 'Software Developer',
+    name: 'Jane Smith'
+  }
+]
